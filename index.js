309c877ef5b3e6353296ebe82e12f04457f488d6
Sean Jackson <seanjackson@Seans-MBP.hsd1.or.comcast.net>
2021-02-14 13:29:53 -0800

    major update to the styling and function

--- index.js
+++ index.js
@@ -1,8 +1,47 @@
-function myFunction() {
-    var x = document.getElementById("page-nav");
-    if (x.className === "page-nav") {
-      x.className += " expanded";
-    } else {
-      x.className = "page-nav";
-    }
-  }
+window.addEventListener('load', () => {
+  const hamburger = document.querySelector('.nav .nav-bar .nav-list .hamburger');
+  const mobile_menu = document.querySelector('.nav .nav-bar .nav-list ul');
+  const nav_item = document.querySelectorAll('.nav .nav-bar .nav-list ul li a');
+  const navigation = document.querySelector('.nav.container');
+  console.log(hamburger)
+});
+
+function hamburgerExpand() {
+  const hamburger = document.querySelector('.nav .nav-bar .nav-list .hamburger');
+  const mobile_menu = document.querySelector('.nav .nav-bar .nav-list ul');
+  hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+};
+
+document.addEventListener('scroll', () => {
+    const navigation = document.querySelector('.nav.container');
+    let scroll_position = window.scrollY;
+        if(scroll_position > 250) {
+            navigation.style.backgroundColor = '#333';
+        } else {
+            navigation.style.backgroundColor = 'transparent';
+        }
+});
+
+function mobileMenuCollapse() {
+    const hamburger = document.querySelector('.nav .nav-bar .nav-list .hamburger');
+    const mobile_menu = document.querySelector('.nav .nav-bar .nav-list ul');
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+};
+
+const links = document.querySelectorAll(".nav-list ul a"); 
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+ 
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+ 
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
